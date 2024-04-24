8518424da8d94dd661d4a9a5d32783b962d1d8f0
Dominic Lastauskas CMPE1666 <honeygooo@hotmail.com>
2024-04-24 09:23:29 -0600

    finished lab3

--- LABs/LabReal3/Sources/main.c
+++ LABs/LabReal3/Sources/main.c
@@ -73,13 +73,17 @@ Segs_Custom(5,0b10001011);
 Segs_Custom(6,0b10110001);
 */
 
-pos = 4;
+pos = 0;
 count = 0;
 pass = 0;
 hex =0;
 dec=0;
 pushed = 0;
 up=0;down=0;left=0;right=0;ctr=0;a=0;b=0;c=0;d=0;
+Segs_Normal(0,a,Segs_DP_ON);
+      Segs_Normal(1,b,Segs_DP_OFF);
+      Segs_Normal(2,c,Segs_DP_OFF);
+      Segs_Normal(3,d,Segs_DP_OFF);
 /********************************************************************/
   // main program loop
 /********************************************************************/
@@ -95,17 +99,127 @@ up=0;down=0;left=0;right=0;ctr=0;a=0;b=0;c=0;d=0;
      else if(SWL_Pushed(SWL_DOWN)){
       down=1;pushed=1;
      }
-     else if(SWL_Pushed(SWL_CTR)){
-      ctr=1;pushed=1;
-     }
      else if(SWL_Pushed(SWL_LEFT)){
       left=1;pushed=1;
      }
      else if(SWL_Pushed(SWL_RIGHT)){
       right=1;pushed=1;
      }
-     
 
+     if(pushed == 0){
+        if(left == 1&& pos > 0){
+          pos--;
+          left = 0;
+        }
+        else if(right == 1 && pos < 3){
+          pos++;
+          right = 0;
+        }
+        switch (pos)
+     {
+     case 0: // edditing the a value leftmoast value
+      if(up ==1){
+          a++;
+          if(a == 10){
+            a = 0;
+          }
+          up = 0;
+        }
+        else if(down == 1){
+          if(a == 0){
+            a = 10;
+          }
+          a--;
+          down = 0;
+        }
+      break;
+     case 1: // edit the b value
+     if(up ==1){
+          b++;
+          if(b == 10){
+            b = 0;
+          }
+          up = 0;
+        }
+        else if(down == 1){
+          if(b == 0){
+            b = 10;
+          }
+          up = 0;
+          b--;
+          down = 0;
+        }
+      break;
+     case 2: // edit the c value
+     if(up ==1){
+          c++;
+          if(c == 10){
+            c = 0;
+          }
+          up = 0;
+        }
+        else if(down == 1){
+          if(c == 0){
+            c = 10;
+          }
+          c--;
+          down = 0;
+        }
+      break;
+     case 3: // edit the d value
+     if(up ==1){
+          d++;
+          if(d == 10){
+            d = 0;
+          }
+          up = 0;
+        }
+        else if(down == 1){
+          if(d == 0){
+            d = 10;
+          }
+          d--;
+          down = 0;
+        }
+      break;
+     default:
+      break;
+     }
+     }
+      pushed = 0;
+     // a method might be needed to put the decimal point on the digit
+     switch (pos)
+     {
+     case 0:
+      Segs_Normal(0,a,Segs_DP_ON);
+      Segs_Normal(1,b,Segs_DP_OFF);
+      Segs_Normal(2,c,Segs_DP_OFF);
+      Segs_Normal(3,d,Segs_DP_OFF);
+      break;
+     case 1:
+      Segs_Normal(0,a,Segs_DP_OFF);
+      Segs_Normal(1,b,Segs_DP_ON);
+      Segs_Normal(2,c,Segs_DP_OFF);
+      Segs_Normal(3,d,Segs_DP_OFF);
+      break;
+      case 2:
+      Segs_Normal(0,a,Segs_DP_OFF);
+      Segs_Normal(1,b,Segs_DP_OFF);
+      Segs_Normal(2,c,Segs_DP_ON);
+      Segs_Normal(3,d,Segs_DP_OFF);
+      break;
+      case 3:
+      Segs_Normal(0,a,Segs_DP_OFF);
+      Segs_Normal(1,b,Segs_DP_OFF);
+      Segs_Normal(2,c,Segs_DP_OFF);
+      Segs_Normal(3,d,Segs_DP_ON);
+      break;
+     default:
+      break;
+     }
+     
+     dec = ((a*1000)+(b*100)+(c*10)+d);
+    Segs_16H(dec,Segs_LineBottom);
 }
 }
 
